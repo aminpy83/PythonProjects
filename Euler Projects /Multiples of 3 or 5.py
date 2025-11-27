24c4496bd33928fdbe39745cc1ec620c28dc7e4b
amin <aminpyss@gmail.com>
2025-11-27 12:33:19 +0330

    first problem done version2

--- Euler Projects /Multiples of 3 or 5.py
+++ Euler Projects /Multiples of 3 or 5.py
@@ -1,6 +1,11 @@
-a = range(0,1001)
-adder = 0
-for i in a:
-    if i % 3 == 0 or i % 5 == 0:
-        adder += i
-print(adder)
+# a = range(0,1001)
+# adder = 0
+# for i in a:
+#     if i % 3 == 0 or i % 5 == 0:
+#         adder += i
+# print(adder)
+
+#--------------------------------------------------
+
+a = sum(range(3,1000,3)) + sum(range(5, 1001, 5)) - sum(range(15, 1001, 15))
+print(a)
