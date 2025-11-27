2ff4d857e17f7ba973dc735535c89d91be39e9a5
amin <aminpyss@gmail.com>
2025-11-27 13:22:18 +0330

    second problem done

---
+++ Euler Projects /Even Fibonacci Numbers.py
@@ -0,0 +1,18 @@
+def fib(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n > 2:
+        return fib(n-1) + fib(n-2)
+    return 0
+
+i = 1
+count = 0
+
+while fib(i) <= 4000000:
+    if fib(i) % 2 == 0:
+        count += fib(i)
+        print('i=',i,'------', fib(i))
+    i += 1
+print(count)
