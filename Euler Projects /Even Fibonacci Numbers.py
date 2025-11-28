662525f21560188593531df21f8178dbd4b2f1cc
amin <aminpyss@gmail.com>
2025-11-28 23:53:46 +0330

    second problem done

--- Euler Projects /Even Fibonacci Numbers.py
+++ Euler Projects /Even Fibonacci Numbers.py
@@ -13,6 +13,6 @@ count = 0
 while fib(i) <= 4000000:
     if fib(i) % 2 == 0:
         count += fib(i)
-        print('i=',i,'------', fib(i))
+        # print('i=',i,'------', fib(i))
     i += 1
 print(count)
