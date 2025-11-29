5130b5c8eca21e0769d4e6173e3c3c78da71a00d
amin <aminpyss@gmail.com>
2025-11-30 00:23:57 +0330

    5th problem ongoing

---
+++ Euler Projects /Smallest Multiple.py
@@ -0,0 +1,8 @@
+x = 1
+while 1:
+    if x % 10 == 0 and x % 9 == 0 and x % 8 == 0 and x % 7 == 0:
+        print(x)
+        break
+    x += 1
+
+# if x % 20 == 0 and x % 19 == 0 and x % 18 == 0 and x % 17 == 0 and x % 15 == 0 and x % 14 == 0 and x % 13 == 0 and x % 11 == 0:
