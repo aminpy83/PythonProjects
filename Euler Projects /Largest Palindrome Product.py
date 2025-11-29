45d0c1a15455a82a388e4b63ae12406557b7e1ed
amin <aminpyss@gmail.com>
2025-11-29 23:52:51 +0330

    4th problem done V2

--- Euler Projects /Largest Palindrome Product.py
+++ Euler Projects /Largest Palindrome Product.py
@@ -1,6 +1,8 @@
 max_num = 0
 for i in range(999,100,-1):
     for j in range(999, 100, -1):
-        if max_num < j*i and str(i * j)[::] == str(i * j)[-1::-1] :
+        if  j*i < max_num :
+            break
+        if str(i * j)[::] == str(i * j)[-1::-1] :
             max_num = i * j
             print(i, j, max_num)
