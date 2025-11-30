f1c9292f38ecc81b6350a13df18f14ab5cae2579
amin <aminpyss@gmail.com>
2025-11-30 12:09:43 +0330

    6th problem done V2

--- Euler Projects /Sum Square Difference.py
+++ Euler Projects /Sum Square Difference.py
@@ -1,7 +1,6 @@
-square_of_sum = sum(range(1, 101)) ** 2
+square_of_sums = sum(range(1, 101)) ** 2
+sum_of_squares = sum (i*i for i in range(1, 101))
+print(abs(square_of_sums - sum_of_squares))
 
-sum_of_squares = 0
-for i in range(1, 101):
-    sum_of_squares += (i ** 2)
-
-print(square_of_sum - sum_of_squares)
+# for i in range(1, 101):
+#     sum_of_squares += (i ** 2)
