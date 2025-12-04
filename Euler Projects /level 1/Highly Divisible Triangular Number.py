152b8b74eb72dc4baf5c1e8b3c19e46c015f2f7d
amin <aminpyss@gmail.com>
2025-12-04 23:07:37 +0330

    12th problem start

---
+++ Euler Projects /level 1/Highly Divisible Triangular Number.py
@@ -0,0 +1,15 @@
+from math import floor
+factors = 1
+x = 1
+
+while True:
+    y = x * (x+1) // 2
+    for i in range(1, y // 2):
+        if y % i == 0:
+            factors += 1
+    if factors > 500:
+        print(x, y, factors)
+        break
+    print(y, factors)
+    factors = 1
+    x += 1
