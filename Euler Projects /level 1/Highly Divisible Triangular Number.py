f1a028ccd13803c1a48282983d432f5d58a0011d
amin <aminpyss@gmail.com>
2025-12-06 00:14:46 +0330

    12th problem done

--- Euler Projects /level 1/Highly Divisible Triangular Number.py
+++ Euler Projects /level 1/Highly Divisible Triangular Number.py
@@ -1,15 +1,27 @@
-from math import floor
+from math import ceil, sqrt
+
 factors = 1
 x = 1
+# y = 0
+
+def is_prime(n):
+    for j in range(2, ceil(sqrt(n))):
+        if n % j == 0:
+            return False
+    return True
 
 while True:
-    y = x * (x+1) // 2          #sum(range(x+1))
-    for i in range(1, (y// 2) +1 ):
-        if y % i == 0:
-            factors += 1
-    if factors > 500:
-        print(x, y, factors)
-        break
-    print(y, factors)
-    factors = 1
-    x += 1
+    y = x * (x + 1) // 2               #y += x           #sum(range(x+1))
+    if is_prime(y):
+        x += 1
+        continue
+    else:
+        for i in range(1, ceil(sqrt(y)) ):
+            if y % i == 0:
+                factors += 2
+        if factors > 500:
+            print(x, y, factors)
+            break
+        print(y, factors)
+        factors = 1
+        x += 1
