0365e408ee1219c989517c6bfc093a35252f566e
amin <aminpyss@gmail.com>
2025-12-05 23:22:16 +0330

    12th problem ongoing

--- Euler Projects /level 1/Highly Divisible Triangular Number.py
+++ Euler Projects /level 1/Highly Divisible Triangular Number.py
@@ -3,8 +3,8 @@ factors = 1
 x = 1
 
 while True:
-    y = x * (x+1) // 2
-    for i in range(1, y // 2):
+    y = x * (x+1) // 2          #sum(range(x+1))
+    for i in range(1, (y// 2) +1 ):
         if y % i == 0:
             factors += 1
     if factors > 500:
