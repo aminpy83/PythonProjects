eabfde10f833ec17c92b4b4b798df918e5f474af
amin <aminpyss@gmail.com>
2025-12-09 00:15:50 +0330

    2nd problem done V2

--- Euler Projects /level 1/Even Fibonacci Numbers.py
+++ Euler Projects /level 1/Even Fibonacci Numbers.py
@@ -1,10 +1,23 @@
+# def fib(n):
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 2
+#     if n > 2:
+#         return fib(n-1) + fib(n-2)
+#     return 0
+
+l = {}
 def fib(n):
     if n == 1:
         return 1
     if n == 2:
         return 2
     if n > 2:
-        return fib(n-1) + fib(n-2)
+        if n in l:
+            return l[n]
+        l[n] = fib(n-1) + fib(n-2)
+        return l[n]
     return 0
 
 i = 1
