dc466860c34001e3a7c6ba8d2c10c7a63d9cdecf
amin <aminpyss@gmail.com>
2025-12-09 11:36:01 +0330

    14th problem done

--- Euler Projects /level 1/Longest Collatz Sequence.py
+++ Euler Projects /level 1/Longest Collatz Sequence.py
@@ -1,4 +1,5 @@
 counter = 0
+max_chain = [0, 0]
 def collatz(n):
     global counter
     counter += 1
@@ -6,11 +7,18 @@ def collatz(n):
         n //= 2
         return collatz(n)
     elif n == 1:
-        return 1
+        return 1, counter
     else:
         n = 3 * n + 1
         return collatz(n)
 
+
 x = 13
 while x < 1_000_000:
     collatz(x)
+    if counter > max_chain[0]:
+        max_chain = counter, x
+    counter = 0
+    x += 1
+    print(x)
+print(max_chain)
