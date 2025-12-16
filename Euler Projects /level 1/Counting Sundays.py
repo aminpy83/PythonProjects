d14f4c7d3d6d1252758ac4ccc4a1ac8527031a23
amin <aminpyss@gmail.com>
2025-12-17 01:36:47 +0330

    debugging

--- Euler Projects /level 1/Counting Sundays.py
+++ Euler Projects /level 1/Counting Sundays.py
@@ -1,19 +1,24 @@
-week = {'sun': 1, 'mon': 2, 'tue' : 3, 'wed': 4, 'tur': 5, 'fri': 6, 'sat': 7}
-months = {'jan': 31, 'feb': 28, 'mar': 31, 'apr': 30, 'may': 31, 'jun': 30,
-          'jul': 31, 'aug': 31, 'sep': 30, 'oct': 31, 'nov': 30, 'dec': 31}
+# week = {'sun': 1, 'mon': 2, 'tue': 3, 'wed': 4, 'tur': 5, 'fri': 6, 'sat': 7}
+months = ['ja31', 'fe28', 'ma31', 'ap30', 'my31', 'jn30', 'ju31', 'au31', 'se30', 'oc31', 'no30', 'de31']
+its_sunday = 0  # 1/0/1901
 count = 0
-its_sunday = 0
 
 # 1 Jan 1900 was a Monday.
 # so first sunday is 7th jan
 
-for i in range(1, 101):                         #years
+for i in range(1, 101):         # years
     if i % 4 == 0:
-        months['feb'].value = 29
-    else :
-        months['feb'].value = 28
-    for j in months.values():                   #months
-        for k in range(1, j+1, 7):              #days
+        months[1] = 'fe29'
+    else:
+        months[1] = 'fe28'
+    for j in months:            # months
+        delta = abs(int(j[2:]) - 28)          # move
+        its_sunday += delta
 
+        last_month =  int(j[2:])
+        if its_sunday > last_month:
+            its_sunday -= last_month
 
-            if k + 7 >  j:
+        if its_sunday == 1:
+            count += 1
+print(count)
